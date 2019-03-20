74f277c68e713bd2b62f07007f71ae7721e728db
wuyuwu <wuyuwu@shuidihuzhu.com>
2019-03-20 21:04:40 +0800

    update

--- src/main/java/proxyPattern/ProxyImage.java
+++ src/main/java/proxyPattern/ProxyImage.java
@@ -1,7 +1,5 @@
 package proxyPattern;
 
-import facadePattern.Shape;
-
 /**
  * @author : wuyuwu@shuidihuzhu.com
  * @Date : 2019/3/20 下午8:53
@@ -18,11 +16,10 @@ public class ProxyImage implements Image {
     public void display() {
         Image image = new ProxyImage("test_10mb.jpg");
 
-        // 图像将从磁盘加载
         image.display();
 
         System.out.println(" ");
-        // 图像不需要从磁盘加载
+
         image.display();
     }
 }

1d28850b06842b7edd78b3c591b50ced75e23a0f
wuyuwu <wuyuwu@shuidihuzhu.com>
2019-03-22 14:05:52 +0800

    update

--- src/main/java/prototypePattern/PrototypePattern.java
+++ src/main/java/prototypePattern/PrototypePattern.java
@@ -3,6 +3,17 @@ package prototypePattern;
 /**
  * @author : wuyuwu@shuidihuzhu.com
  * @Date : 2019/3/10 下午4:28
+ *
+ *  TODO
+ * 代理模式：
+ *
+ * 使用场景：
+ *
+ *
+ * 优缺点：
+ *
+ *
+ *
  */
 public class PrototypePattern {
     public static void main(String[] args) {
